0435038bb1617d38283822cf0ce74f4236d0da21
Tim Lee <timothy.t.lee@gmail.com>
2017-07-21 17:31:31 +0800

    Add ignite

--- App.js
+++ App.js
@@ -1,23 +1,19 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import './App/Config/ReactotronConfig'
+import React from 'react'
+// import {StyleSheet, Text, View} from 'react-native';
+import IgniteApp from './App/Containers/App'
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+  render () {
+    return <IgniteApp />
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
